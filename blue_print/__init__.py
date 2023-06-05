d2d7d8b3b1de9ce47c81318cb1ad839322ef6de7
luowenxue <1056084639@qq.com>
2023-06-05 08:26:12 +0800

    已完成cudr

--- blue_print/__init__.py
+++ blue_print/__init__.py
@@ -1,14 +1,16 @@
 import os
 from flask import (
-    Flask, render_template, session, redirect
+    Flask, render_template, session, redirect, jsonify
 )
+from . import db
+
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.secret_key = 'asasfsfsddssd'
 
     app.config.from_mapping(
-        SECRET_KEY = 'dev',
+        SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'book.sqlite'),
     )
 
@@ -19,20 +21,27 @@ def create_app(test_config=None):
         # load the test config if passed in
         app.config.from_mapping(test_config)
 
-    try: 
+    try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
 
-    
     from .views.auth import auth
 
     @app.route('/index')
     @auth
     def index():
-        return  render_template('index.html')
+        conection = db.get_db()
+        query = 'SELECT * FROM book ORDER BY created DESC'
+        cursor = conection.execute(query, ())
+        books = cursor.fetchall()
 
-    
+        result_books = []
+        for item in books:
+            result_books.append({k: item[k] for k in item.keys()})
+        cursor.close()
+        return render_template('index.html', result_books=result_books)
+
     from .views import user
     from .views import sigin
     from .views import book
@@ -41,7 +50,6 @@ def create_app(test_config=None):
     app.register_blueprint(user.bp, url_prefix='/user')
     app.register_blueprint(book.bp, url_prefix='/book')
 
-    from . import db
     db.init_app(app)
 
     return app
--- blue_print/views/book.py
+++ blue_print/views/book.py
@@ -1,26 +1,115 @@
 from flask import (
     Blueprint, render_template, request, redirect, url_for, session
 )
-import sqlite3 as sql
+from datetime import datetime
+from .. import db
 
 
 bp = Blueprint('book', __name__)
 
-@bp.route('/add', methods=['GET','POST'])
+
+def query_db(query, args=(), one=False):
+    cur = db.get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+
+@bp.route('/add', methods=['GET', 'POST'])
 def add():
     if request.method == 'GET':
         return render_template('addbook.html')
-    
-    # try:
-    #     book_name = request.form.get('book_name')
-    #     author_name = request.form.get('author_name')
-    #     intro = request.form.get('intro')
-    #     created = request.form.get('created')
+    try:
+        book_name = request.form.get('book_name')
+        author_name = request.form.get('author_name')
+        intro = request.form.get('intro')
+        # created = request.form.get('created')
+        datetime_str = request.form.get('created')
+        datetime_obj = datetime.fromisoformat(datetime_str)
+        created = datetime_obj.strftime('%Y-%m-%d %H:%M:%S')
+        author_id = 1988
 
-    #     with sql.connect() as con:
-    # except:
-    #     con.rollback()
-    #              msg = "error in insert operation"
+        # 1、写sql 语句
+        query = 'INSERT INTO book (author_id, author_name, created, name, intro) VALUES(?, ?, ?, ?, ?)'
+        # 2、获取数据库连接
+        conection = db.get_db()
+        # 3、获取游标，执行sql
+        args = [author_id, author_name, created, book_name, intro]
+        cursor = conection.execute(query, args)
+        # 对数据库有改动，commit提交，不然不生效
+        conection = conection.commit()
+        # result = query_db(query,[author_id, author_name, created, book_name, intro], one=False)
+        # 关闭连接
+        cursor.close()
+        msg = "您成功添加一本书。"
+        assert cursor.rowcount == 1, "添加失败，添加过程出错了。"
+    except Exception as e:
+        msg = "添加失败，添加过程出错了：" + str(e)
+        return render_template('result.html', msg=msg)
+    else:
+        return render_template('result.html', msg=msg)
 
-    # finally:
-        
+
+@bp.route('/book', methods=['GET', 'POST', 'DELETE', 'PUT'])
+def book():
+    if request.method == 'GET':
+        id = request.args.get('id')
+        query = "SELECT * FROM book WHERE id = ?"
+        book = query_db(query, [id], one=True)
+        print(book['created'])
+        result = {k: book[k] for k in book.keys()}
+        return result
+    elif request.method == 'DELETE':
+        id = request.args.get('id')
+        query = "DELETE FROM book WHERE id = ?"
+        try:
+            conection = db.get_db()
+            args = [id]
+            cursor = conection.execute(query, args)
+            conection.commit()
+            cursor.close()
+            msg = '您成功删除了一条数据。'
+            assert cursor.rowcount == 1, '删除失败'
+        except Exception as e:
+            msg = "添加失败，添加过程出错了：" + str(e)
+            return render_template('result.html', msg=msg)
+        else:
+            return render_template('result.html', msg=msg)
+    return '1'
+
+
+@bp.route('/editing/<int:id>', methods=['GET', 'POST'])
+def editing(id):
+    if request.method == 'GET':
+        query = "SELECT * FROM book WHERE id = ?"
+        book = query_db(query, [id], one=True)
+        book = {k: book[k] for k in book.keys()}
+        print(book)
+        return render_template('editing.html', book=book)
+
+    try:
+        book_name = request.form.get('book_name')
+        author_name = request.form.get('author_name')
+        intro = request.form.get('intro')
+        datetime_str = request.form.get('created')
+        datetime_obj = datetime.fromisoformat(datetime_str)
+        created = datetime_obj.strftime('%Y-%m-%d %H:%M:%S')
+
+        query = 'UPDATE book SET author_name = ?, created = ?, name = ?, intro = ? WHERE id = ?'
+        # 2、获取数据库连接
+        conection = db.get_db()
+        # 3、获取游标，执行sql
+        args = [author_name, created, book_name, intro, id]
+        cursor = conection.execute(query, args)
+        # 对数据库有改动，commit提交，不然不生效
+        conection = conection.commit()
+        # 关闭连接
+        cursor.close()
+
+        msg = "成功编辑了一本书"
+        assert cursor.rowcount == 1, "保存失败，添加过程出错了。"
+    except Exception as e:
+        msg = "保存失败，保存过程出错了：" + str(e)
+        return render_template('result.html', msg=msg)
+    else:
+        return render_template('result.html', msg=msg)
