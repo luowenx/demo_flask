3fae0b593c0e11ea048f0f998bb934bc6242e9ce
luowenxue <1056084639@qq.com>
2023-06-07 22:43:38 +0800

    ..

--- blue_print/db.py
+++ blue_print/db.py
@@ -27,6 +27,12 @@ def init_db():
     with current_app.open_resource('schema.sql') as f:
         db.executescript(f.read().decode('utf8'))
 
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
 
 @click.command('init-db')
 def init_db_command():
